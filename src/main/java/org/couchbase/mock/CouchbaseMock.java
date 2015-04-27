dc5ea8dcbe3ccdadd5707231f1c26a684199ee5c
candide guevara <candide-guevara@users.noreply.github.com>
2015-04-27 15:49:29 +0200

    Fixed typo in help message

--- src/main/java/org/couchbase/mock/CouchbaseMock.java
+++ src/main/java/org/couchbase/mock/CouchbaseMock.java
@@ -407,8 +407,9 @@ public class CouchbaseMock {
 
         o.printf("Options are:%n");
         o.printf("-h --host             The hostname for the REST port. Default=8091%n");
+        o.printf("-p --port             The REST port. Default=8091%n");
         o.printf("-b --buckets          (See description below%n");
-        o.printf("-p --nodes            The number of nodes each bucket should contain. Default=%d%n", defaultConfig.numNodes);
+        o.printf("-n --nodes            The number of nodes each bucket should contain. Default=%d%n", defaultConfig.numNodes);
         o.printf("-v --vbuckets         The number of vbuckets each bucket should contain. Default=%d%n", defaultConfig.numVBuckets);
         o.printf("-R --replicas         The number of replica nodes for each bucket. Default=%d%n", defaultConfig.numReplicas);
         o.printf("   --harakiri-monitor The host:port on which the control socket should connect to%n");
