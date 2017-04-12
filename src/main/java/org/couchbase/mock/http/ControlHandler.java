afd2c9dd7a5a1773332a26906d9c573887df0ceb
Mark Nunberg <mnunberg@haskalah.org>
2017-04-12 08:26:40 -0700

    http/ControlHandler: Better parsing of request payload body
    For one-off requests, you can now send the request as form-encoded or
    JSON, in addition to simply encoding it within the URL.
    
    Change-Id: I0ce16ff954527be077815890e3614fde38287515
    Reviewed-on: http://review.couchbase.org/76910
    Tested-by: Mark Nunberg <mark.nunberg@couchbase.com>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- src/main/java/org/couchbase/mock/http/ControlHandler.java
+++ src/main/java/org/couchbase/mock/http/ControlHandler.java
@@ -14,16 +14,19 @@
  * limitations under the License.
  */
 package org.couchbase.mock.http;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.io.*;
 import java.net.URL;
 import java.net.URLDecoder;
 
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+import org.apache.http.*;
+import org.apache.http.entity.ContentType;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpRequestHandler;
+import org.apache.http.util.EntityUtils;
 import org.couchbase.mock.control.CommandNotFoundException;
 import org.couchbase.mock.control.MockCommandDispatcher;
 import org.couchbase.mock.control.handlers.MockHelpCommandHandler;
@@ -51,6 +54,39 @@ public class ControlHandler implements HttpRequestHandler {
         response.setStatusCode(code);
     }
 
+    private JsonObject getQueryPayload(URL url, HttpRequest request) throws HttpException, IOException {
+        // First check the URL itself to see if there's a payload
+        JsonObject payload;
+        payload = HandlerUtil.getJsonQuery(url);
+        if (payload != null) {
+            return payload;
+        }
+
+        // Next, check the actual request body..
+        if (!(request instanceof HttpEntityEnclosingRequest)) {
+            return null;
+        }
+
+        String encoded = EntityUtils.toString(((HttpEntityEnclosingRequest)request).getEntity());
+        // Determine how to parse this..
+        Header header = request.getLastHeader(HttpHeaders.CONTENT_TYPE);
+        if (header == null || header.getValue().equals(ContentType.APPLICATION_FORM_URLENCODED.getMimeType())) {
+            // Parse as URL:
+            url = new URL("http://foo.com?" + encoded);
+            return HandlerUtil.getJsonQuery(url);
+        } else if (header.getValue().equals(ContentType.APPLICATION_JSON.getMimeType())) {
+            JsonParser parser = new JsonParser();
+            JsonElement elem = parser.parse(encoded);
+            if (elem != null) {
+                return elem.getAsJsonObject();
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
     @Override
     public void handle(HttpRequest request, HttpResponse response, HttpContext context) throws HttpException, IOException {
         URL url = HandlerUtil.getUrl(request);
@@ -68,7 +104,7 @@ public class ControlHandler implements HttpRequestHandler {
                 throw new WantHelpException();
             }
 
-            JsonObject payload = HandlerUtil.getJsonQuery(url);
+            JsonObject payload = getQueryPayload(url, request);
             String cmdStr = URLDecoder.decode(components[1], "UTF-8");
             String rStr = dispatcher.dispatch(cmdStr, payload).toString();
             HandlerUtil.makeJsonResponse(response, rStr);
