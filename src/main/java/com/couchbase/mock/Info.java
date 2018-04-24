b2fac8d29091dd055bc0ef2564b817fd366e8e8b
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-04-25 02:59:13 +0300

    Bump version to 1.5.19
    Change-Id: Ia7317e64f5e830e79328dbe5a0d5992b38b13895
    Reviewed-on: http://review.couchbase.org/93252
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/main/java/com/couchbase/mock/Info.java
+++ src/main/java/com/couchbase/mock/Info.java
@@ -22,7 +22,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * about the Couchbase Mock
  */
 public final class Info {
-    private static final String VERSION = "1.5.18";
+    private static final String VERSION = "1.5.19";
 
     /**
      * get major version
