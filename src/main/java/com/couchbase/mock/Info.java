015dd61506c316124cb22cbdc5629dcff19061c4
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-12-13 23:01:28 +0300

    Bump version to 1.5.20
    Change-Id: I210167d380f7e4eb5c8c3011be0da6f056cba58b
    Reviewed-on: http://review.couchbase.org/102811
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/main/java/com/couchbase/mock/Info.java
+++ src/main/java/com/couchbase/mock/Info.java
@@ -23,7 +23,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * about the Couchbase Mock
  */
 public final class Info {
-    private static final String VERSION = "1.5.19";
+    private static final String VERSION = "1.5.20";
     private static final AtomicInteger configRevision = new AtomicInteger(1);
     private static final AtomicLong clockOffset = new AtomicLong();
 
